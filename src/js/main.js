cfcd699970f2aa4468bf0ba288eac169f0fcbb4d
mmerabet <mhmd.merabet@gmail.com>
2024-07-29 21:40:06 +0200

    cleaning code

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-// Fix this Damisa: the toggle doesn't doesn't work properly, ask me
-// to show you what is wrong with this toggle
 const dropDownButton = document.getElementById('test')
 const dropDownMenu = document.getElementById('dropdown-menu-bar-2');
 const dropDownContainer = document.getElementById('dropdown-menu-bar-2-button');
